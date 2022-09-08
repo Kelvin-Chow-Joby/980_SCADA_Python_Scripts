11d7814422187d24e7c342b17cac9fd5d3e19be0
Kellen Murray <kellen@konductive.io>
2022-09-08 16:40:22 -0700

    Added check for existing tickets

--- jira_ION.py
+++ jira_ION.py
@@ -1,11 +1,9 @@
+#Documentation available here: https://developer.atlassian.com/server/jira/platform/jira-rest-api-examples/
+from jira import JIRA
+
 #-------------------------------------------------------------------------------
 # SECTION: Arguments
 #-------------------------------------------------------------------------------
-import sys
-#This is where we define the issue information
-projectKey = sys.argv[1]
-partNumber = sys.argv[2]
-quantity = sys.argv[3]
 
 #Example issue information:
 #projectKey = 'PEP' #This would be the target project key
@@ -13,52 +11,88 @@ quantity = sys.argv[3]
 #partNumber = '505123-001' #Put the target part number here - THIS NEEDS TO GET POPULATED BY A QUERY
 
 #-------------------------------------------------------------------------------
-# SECTION: Jira
+# Setup JIRA connection
 #-------------------------------------------------------------------------------
-#Documentation available here: https://developer.atlassian.com/server/jira/platform/jira-rest-api-examples/
-from jira import JIRA
-#This is where we define the connection information
+
 jiraServer = 'https://jira.na.joby.aero' #Link to the JIRA server
 workspaceEmail = 'nicholas.morris@joby.aero' #Authenticated user of choice
 apiToken = 'NDI5MzQzOTEwMDY5Otq8cfEGPkJZfTjrSfl8H0Z0s/3G' #Needs to be created by an authenticated user at https://id.atlassian.com/manage-profile/security/api-tokens
-# This is where we open the connection object
-jira_connection = JIRA(
+
+# This is where we create the connection object
+jiraConnection = JIRA(
                         token_auth=apiToken,
                         server=jiraServer
                         )
-#The following are the example methods
-#Example that creates a single issue
-issue_dict = {
-                'project': {'key': 'PEP'},
-                'summary': partNumber + ' - Inventory Depleted',
-                'description': 'Inventory is depleted on p/n ' + partNumber + ', please order ' + quantity + ' more',
-                'issuetype': {'name': 'Problem'},
-                "customfield_11502": partNumber, #Custom field for 'part number'
-                "customfield_16400": { "value": "Inventory" } #Custom field for 'problem type' dropdown
-                }
-new_issue = jira_connection.create_issue(fields=issue_dict) #Actual method call
-#Example that creates multiple issues at once
-issue_list = [
-    {
-        'project': {'key': projectKey},
-        'summary': 'Test summary 1',
-        'description': 'Test description 1',
-        'issuetype': {'name': issueType},
-    },
-    {
-        'project': {'key': projectKey},
-        'summary': 'Test summary 2',
-        'description': 'Test description 2',
-        'issuetype': {'name': issueType},
-    },
-    {
-        'project': {'key': projectKey},
-        'summary': 'Test summary 3',
-        'description': 'Test description 3',
-        'issuetype': {'name': issueType},
-    }]
-    
+
+
 #-------------------------------------------------------------------------------
-# SECTION: Method Calls
+# Search for existing issue
 #-------------------------------------------------------------------------------
-jira_connection.create_issues(field_list=issue_dict)
+def searchForIssue(partNumber: str):
+    jqlString = 'project = "PEP" AND "Part Number[Short text]" ~ "%s" AND status NOT IN ("In Progress","Done") ORDER BY created DESC' % partNumber
+    print('Running check for issue with part number ' + partNumber)
+    #print('JQL query: ' + jqlString)
+    getIssues = jiraConnection.search_issues(jqlString)
+    return getIssues
+
+
+#-------------------------------------------------------------------------------
+# Create single issue
+#-------------------------------------------------------------------------------
+def createSingleIssue(partNumber: str, quantity: int):
+    issueDict = {
+                    'project': {'key': 'PEP'},
+                    'summary': partNumber + ' - Inventory Depleted',
+                    'description': 'Inventory is depleted on p/n ' + partNumber + ', please order ' + str(quantity) + ' more',
+                    'issuetype': {'name': 'Problem'},
+                    "customfield_11502": partNumber, #Custom field for 'part number'
+                    "customfield_16400": { "value": "Inventory" } #Custom field for 'problem type' dropdown
+                    }
+                    
+    issueCheck = searchForIssue(partNumber=partNumber)
+
+    if len(issueCheck) == 0:
+        print('No existing issue found, creating new issue')
+        postSingleIssue = jiraConnection.create_issue(fields=issueDict) #Actual method call
+        print('New issue created: ' + str(postSingleIssue))
+        return postSingleIssue
+    else:
+        issueKey = issueCheck[0].key
+        print('Open ticket already exists with a matching part number: ' + str(issueKey))
+        return
+
+
+#-------------------------------------------------------------------------------
+# Create multiple issues
+#-------------------------------------------------------------------------------
+# def createMultipleIssues(issueList):
+#     issue_list = [
+#                     {
+#                         'project': {'key': 'PEP'},
+#                         'summary': partNumber + ' - Inventory Depleted',
+#                         'description': 'Inventory is depleted on p/n ' + partNumber + ', please order ' + str(quantity) + ' more',
+#                         'issuetype': {'name': 'Problem'},
+#                         "customfield_11502": partNumber, #Custom field for 'part number'
+#                         "customfield_16400": { "value": "Inventory" } #Custom field for 'problem type' dropdown
+#                     },
+#                     {
+#                         'project': {'key': 'PEP'},
+#                         'summary': partNumber + ' - Inventory Depleted',
+#                         'description': 'Inventory is depleted on p/n ' + partNumber + ', please order ' + str(quantity) + ' more',
+#                         'issuetype': {'name': 'Problem'},
+#                         "customfield_11502": partNumber, #Custom field for 'part number'
+#                         "customfield_16400": { "value": "Inventory" } #Custom field for 'problem type' dropdown
+#                     },
+#                     {
+#                         'project': {'key': 'PEP'},
+#                         'summary': partNumber + ' - Inventory Depleted',
+#                         'description': 'Inventory is depleted on p/n ' + partNumber + ', please order ' + str(quantity) + ' more',
+#                         'issuetype': {'name': 'Problem'},
+#                         "customfield_11502": partNumber, #Custom field for 'part number'
+#                         "customfield_16400": { "value": "Inventory" } #Custom field for 'problem type' dropdown
+#                     }
+#                     ]
+        
+#     postMultipleIssues = jiraConnection.create_issues(field_list=issueList)
+#     print('postMultipleIssues response: ' + postMultipleIssues)
+#     return postMultipleIssues
